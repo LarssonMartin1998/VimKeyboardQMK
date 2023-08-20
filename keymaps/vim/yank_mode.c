4d99b5159858b1d75e17b1f50b5f81ad27c79dcc
Martin Larson <larssonmartin1998@gmail.com>
2023-08-21 00:15:00 +0200

    Add working yank mode, potential canditate for 1.0 (feature complete)

--- keymaps/vim/yank_mode.c
+++ keymaps/vim/yank_mode.c
@@ -23,10 +23,14 @@ void command_yank_to_end_of_document(void);
 void perform_copy(void);
 
 void yank_mode_process_keycode(uint16_t keycode) {
+    bool performed_command = false;
+
     if (goto_vimlayer_process_keycode(keycode)) {
         if (wants_to_go_to_beginning_of_document()) {
+            performed_command = true;
             command_yank_to_beginning_of_document();
         } else if (wants_to_go_to_end_of_document()) {
+            performed_command = true;
             command_yank_to_end_of_document();
         }
 
@@ -36,47 +40,60 @@ void yank_mode_process_keycode(uint16_t keycode) {
     switch (keycode) {
         case KC_4:
             command_yank_line_forwards();
+            performed_command = true;
             break;
         case KC_6:
         case KC_0:
             command_yank_line_backwards();
+            performed_command = true;
             break;
         case KC_Y:
             command_yank_current_line();
+            performed_command = true;
             break;
         case KC_I:
             activate_inside_mode_for_next_command();
             break;
         case KC_H:
             command_yank_left();
+            performed_command = true;
             break;
         case KC_J:
             command_yank_downwards();
+            performed_command = true;
             break;
         case KC_K:
             command_yank_upwards();
+            performed_command = true;
             break;
         case KC_L:
             command_yank_right();
+            performed_command = true;
             break;
         case KC_W:
             if (is_command_inside()) {
                 command_yank_in_word();
+                performed_command = true;
             } else {
                 command_yank_to_next_word();
+                performed_command = true;
             }
 
             break;
         case KC_B:
             command_yank_to_beginning_of_word();
+            performed_command = true;
             break;
         case KC_E:
             command_yank_to_end_of_word();
+            performed_command = true;
             break;
     }
 
-    skip_switchcase:
-    reset_data();
+skip_switchcase:
+    if (performed_command) {
+        reset_data();
+    }
 }
 
 void command_yank_line_forwards(void) {
@@ -125,9 +142,19 @@ void command_yank_current_line(void) {
 }
 
 void command_yank_left(void) {
+    register_code(KC_LSFT);
+    command_left();
+    unregister_code(KC_LSFT);
+    perform_copy();
+    tap_code(KC_LEFT);
 }
 
 void command_yank_right(void) {
+    register_code(KC_LSFT);
+    command_right();
+    unregister_code(KC_LSFT);
+    perform_copy();
+    tap_code(KC_RIGHT);
 }
 
 void command_yank_to_next_word(void) {
